19cada5e3b760d119c84dd886f61fcb14da759e2
Ilya <zaytsevilia.a@gmail.com>
2022-08-18 14:35:11 +0300

    homework_lesson_7

---
+++ lesson_7/common/variables.py
@@ -0,0 +1,17 @@
+DEFAULT_PORT = 7777
+ENCODING = 'utf-8'
+MAX_PACKAGE_LENGTH = 1024
+DEFAULT_IP_ADDRESS = '127.0.0.1'
+MAX_CONNECTIONS = 5
+
+ACTION = 'action'
+TIME = 'time'
+USER = 'user'
+ACCOUNT_NAME = 'account_name'
+SENDER = 'sender'
+
+PRESENCE = 'presence'
+RESPONSE = 'response'
+ERROR = 'error'
+MESSAGE = 'message'
+MESSAGE_TEXT = 'message_text'
